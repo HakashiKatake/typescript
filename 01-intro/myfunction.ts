9e4e11750977befe19c175971a2d7c1ea731442a
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-07-27 21:02:24 +0530

    void, never and better typesafety

--- 01-intro/myfunction.ts
+++ 01-intro/myfunction.ts
@@ -1,5 +1,7 @@
-function addTwo(num: number) {
-    return num + 2;
+function addTwo(num: number): number {
+    //return num + 2;
+    // return "hello"; // this will not work in ts
+    return num + 2; // this will work fine
 }
 
 function getUpper(val: string) {
@@ -8,9 +10,16 @@ function getUpper(val: string) {
 
 function signUpUser(name: string, email: string, password: string, isPaid: boolean) {
     console.log(`User signed up with name: ${name}, email: ${email}, password: ${password}, isPaid: ${isPaid}`);
-
-
 }
+
+// function getValue(myval: number) : boolean {
+//     if (myval > 5) {
+//         return true;
+//     }
+//     return "200 OK"; // this will not work in ts, as it expects a boolean return type
+    
+
+// }
 
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
 
@@ -25,6 +34,33 @@ signUpUser("Saurabh", "saurabh@example.com", "password123", true); // this will 
 
 loginUser("Saurabh", "saurabh@example.com", true); 
 // loginUser("Saurabh", "saurabh@example.com", 5); // this will not work in ts
+
+
+
+const getHello = (s: string) : string => {
+    return ''
+}
+
+//const heros = ["thor", "spiderman", "ironman"];
+const heros = [1,2,3]
+
+heros.map((hero): string => {
+    return `Hero is ${hero}`;
+    //return 1  this will not work in ts, as it expects a string return type
+});
+
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+    
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
+
+
+
 
 export{};
 
