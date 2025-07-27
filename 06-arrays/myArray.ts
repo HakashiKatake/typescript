ef18862e48fd2ec4ba6333f5ef54462a3e53b0b0
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-07-27 21:27:01 +0530

    arrays in ts

---
+++ 06-arrays/myArray.ts
@@ -0,0 +1,27 @@
+const superHeroes: string[] = []
+//const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    //'[255, 255, 0]' // This is a string, not a number array
+]
+
+
+
+
+
+superHeroes.push("Iron Man")
+heroPower.push(100)
+
+allUsers.push({name: "Saurabh", isActive: true})
+
