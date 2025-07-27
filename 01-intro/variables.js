29c154e8b93ed6682a5f9f12442991fb42cc0fa4
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-07-27 20:44:13 +0530

    boolean and numbers

--- 01-intro/variables.js
+++ 01-intro/variables.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Saurabh";
+var mynum = 6;
+greetings.toLowerCase(); // This will work fine
 console.log(greetings);
+//number 
+var userId = 33432; // this is good but we can avoid it by using let userId = 33432;
+//boolean 
+var isLoggedIn = false;
--- 01-intro/variables.ts
+++ 01-intro/variables.ts
@@ -5,5 +5,15 @@ let mynum = 6
 greetings.toLowerCase(); // This will work fine
 console.log(greetings);
 
+//number 
+
+let userId: number = 33432; // this is good but we can avoid it by using let userId = 33432;
+
+//boolean 
+let isLoggedIn: boolean = false;
+
+
+
+
 
 export{};
