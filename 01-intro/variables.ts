993c7644eb203c62a0a30296c181f3758d9a13c4
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-07-27 20:47:17 +0530

    any key word

--- 01-intro/variables.ts
+++ 01-intro/variables.ts
@@ -12,7 +12,16 @@ let userId: number = 33432; // this is good but we can avoid it by using let use
 //boolean 
 let isLoggedIn: boolean = false;
 
+//any 
 
+let hero: string;
+// This will give an error if strict mode is enabled, but it will work in JavaScript
+let hero1: any; // This is not recommended, but it will work
+function getHero() {
+    return "thor";
+}
+
+hero = getHero(); // This will work fine, but it's better to use a specific type
 
 
 
